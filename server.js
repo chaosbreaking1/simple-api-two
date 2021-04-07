e038a22600179dce394decb2d0a73b2d7ffbcd30
chaosbreaking1 <58153006+chaosbreaking1@users.noreply.github.com>
2021-04-06 23:37:58 -0700

    added port again

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ app.get('/api/rappers/:rapperName', (request, response) => {
   }
 })
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
 })
